344048342775b09dc759147db14b8a998c1af4bf
eBusiness security <eBus@eBusinesss-MacBook-Pro.local>
2017-05-04 11:20:07 +0300

    merge

---
+++ ios/react_native_navigation_bootstrap/AppDelegate.m
@@ -0,0 +1,27 @@
+#import "AppDelegate.h"
+#import <RNAnalytics/RNAnalytics.h>
+
+#import "RCCManager.h"
+#import <React/RCTRootView.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  NSURL *jsCodeLocation;
+
+  [RNAnalytics registerWithInitiallyEnabled:true];  // Initialize Mobile Center analytics
+#ifdef DEBUG
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.backgroundColor = [UIColor whiteColor];
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation];
+  
+  return YES;
+}
+
+@end
