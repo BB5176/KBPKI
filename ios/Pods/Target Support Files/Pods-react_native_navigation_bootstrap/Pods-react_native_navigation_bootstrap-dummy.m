ddb26a322fa78e8df4da562a12178716ef17d40d
eBusiness security <eBus@eBusinesss-MacBook-Pro.local>
2017-05-04 11:19:57 +0300

    merge

--- ios/Pods/Target Support Files/Pods-react_native_navigation_bootstrap/Pods-react_native_navigation_bootstrap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_react_native_navigation_bootstrap : NSObject
-@end
-@implementation PodsDummy_Pods_react_native_navigation_bootstrap
-@end
